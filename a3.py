7510d9365e83316897c5091ad997eca3d3fe6243
Charles Liu <cliu2014@mit.edu>
2013-09-24 02:49:55 -0400

    Works except YUV filter

---
+++ a3.py
@@ -0,0 +1,172 @@
+import numpy as np
+from scipy import ndimage
+import scipy
+from scipy import signal
+from numpy import exp
+import imageIO as io
+import math
+
+def check_module():
+    return 'Charles Liu'
+
+def boxBlur(im, k):
+    # Return a  blured image filtered by box filter
+    shift = int(k/2)
+    factor = 1.0 / (k**2)
+    im_out = io.constantIm(im.shape[0], im.shape[1], 0)
+    for y, x in imIter(im_out):
+        for yp, xp in [(a, b) for a in xrange(k) for b in xrange(k)]:
+            im_out[y, x] += getEdgePadded(im, y + yp - shift, x + xp - shift) * factor
+    return im_out
+
+def convolve(im, kernel):
+    # Return an image filtered by kernel
+    shiftY, shiftX = (int(kernel.shape[0] / 2), int(kernel.shape[1] / 2))
+    im_out = io.constantIm(im.shape[0], im.shape[1], 0)
+    for y, x in imIter(im_out):
+        for yp, xp in imIter(kernel):
+            im_out[y, x] += getEdgePadded(im, y + yp - shiftY, x + xp - shiftX) * kernel[yp, xp]
+    return im_out
+
+def gradientMagnitude(im):
+    # Return the sum of the absolute value of the gradient
+    # The gradient is the filtered image by Sobel filter
+    s_X = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    s_Y = np.transpose(s_X)
+    G_X = convolve(im, s_X)
+    G_Y = convolve(im, s_Y)
+    return (G_X**2 + G_Y**2) ** 0.5
+
+def horiGaussKernel(sigma, truncate=3):
+    # Return an one d kernel
+    offset = int(math.ceil(truncate * sigma))
+    kernel = [math.e ** -((i - offset)**2 / float(2 * sigma**2)) for i in xrange(2 * offset + 1)]
+    return np.array([kernel]) / sum(kernel)
+
+def gaussianBlur(im, sigma, truncate=3):
+    horizKernel = horiGaussKernel(sigma, truncate)
+    return convolve(convolve(im, horizKernel), np.transpose(horizKernel))
+
+def gauss2D(sigma=2, truncate=3):
+    # Return an 2-D array of gaussian kernel
+    horizKernel = horiGaussKernel(sigma, truncate)
+    return np.dot(np.transpose(horizKernel), horizKernel)
+
+def unsharpenMask(im, sigma, truncate, strength):
+    return im + strength * (im - gaussianBlur(im, sigma, truncate))
+
+def bilateral(im, sigmaRange, sigmaDomain):
+    truncate = 3
+    offset = int(math.ceil(truncate * sigmaDomain))
+    gaussianDistDomain = gauss2D(sigmaDomain, truncate)
+    im_out = io.constantIm(im.shape[0], im.shape[1], 0)
+    for y, x in imIter(im_out):
+        k = 0
+        s = np.empty([3])
+        for yp in xrange(y - offset, y + offset + 1):
+            for xp in xrange(x - offset, x + offset + 1):
+                # Get domain Gaussian
+                domainGaussian = 0
+                if abs(y - yp) < offset and abs(x - xp) < offset:
+                    domainGaussian = gaussianDistDomain[y - yp + offset, x - xp + offset]
+
+                # Get range Gaussian
+                imDist = np.sum((im[y, x] - getEdgePadded(im, yp, xp))**2) ** 0.5
+                rangeGaussian = continuousGaussian(imDist, sigmaRange)
+
+                k += domainGaussian * rangeGaussian
+                s += domainGaussian * rangeGaussian * getEdgePadded(im, yp, xp)
+
+        im_out[y, x] = s / k
+
+    return im_out
+
+def bilaYUV(im, sigmaRange, sigmaY, sigmaUV):
+    # 6.865 only: filter YUV differently
+
+    imYUV = rgb2yuv(im)
+    truncate = 3
+    offsetY = int(math.ceil(truncate * sigmaY))
+    offsetUV = int(math.ceil(truncate * sigmaUV))
+    offset = max(offsetY, offsetUV)
+
+    gaussianDistY = gauss2D(sigmaY, truncate)
+    gaussianDistUV = gauss2D(sigmaUV, truncate)
+    im_out = io.constantIm(im.shape[0], im.shape[1], 0)
+
+    for y, x in imIter(im_out):
+        kY = 0
+        kUV = 0
+        s = np.empty([3])
+        for yp in xrange(y - offset, y + offset + 1):
+            for xp in xrange(x - offset, x + offset + 1):
+                # Get domain Gaussian
+                gaussianUV = 0
+                gaussianY = 0
+                if abs(y - yp) < offsetY and abs(x - xp) < offsetY:
+                    gaussianY = gaussianDistY[y - yp + offsetY, x - xp + offsetY]
+
+                if abs(y - yp) < offsetUV and abs(x - xp) < offsetUV:
+                    gaussianUV = gaussianDistUV[y - yp + offsetUV, x - xp + offsetUV]
+
+                # Get range Gaussian
+
+                imPadded = getEdgePadded(imYUV, yp, xp)
+
+                imDist = np.sum((imYUV[y, x] - imPadded)**2) ** 0.5
+                rangeGaussian = continuousGaussian(imDist, sigmaRange)
+
+                kY += gaussianY * rangeGaussian
+                kUV += gaussianUV * rangeGaussian
+
+                s[0] += gaussianY * rangeGaussian * imPadded[0]
+                s[1] += gaussianUV * rangeGaussian * imPadded[1]
+                s[2] += gaussianUV * rangeGaussian * imPadded[2]
+
+        s[0] /= kY
+        s[1] /= kUV
+        s[2] /= kUV
+
+        im_out[y, x] = s
+
+    return yuv2rgb(im_out)
+
+# Helpers
+
+def continuousGaussian(x, sigma):
+    return 1.0 / math.sqrt(2 * math.pi * sigma**2) * math.e ** -(x**2 / (2 * sigma**2))
+
+def imIter(im):
+    for y in range(0,im.shape[0]):
+        for x in range(0,im.shape[1]):
+            yield (y,x)
+
+def clipX(im, x):
+    return min(im.shape[1] - 1, max(x, 0))
+
+def clipY(im, y):
+    return min(im.shape[0] - 1, max(y, 0))
+
+def getEdgePadded(im, y, x):
+    return im[clipY(im, y), clipX(im, x)];
+
+def rgb2yuv(im):
+    imyuv = im.copy()
+    M = np.array([[0.299, 0.587, 0.114], [-0.14713, -0.28886, 0.436], [0.615, -0.51499, -0.1001]])
+    (height, width, rgb) = np.shape(imyuv)
+    for y in xrange(height):
+        for x in xrange(width):
+            imyuv[y][x] = np.dot(M, imyuv[y][x])
+    return imyuv
+
+def yuv2rgb(im):
+    imrgb = im.copy()
+    M = np.matrix([[1, 0, 1.13983], [1, -0.39465, -0.58060], [1, 2.03211, 0]])
+    (height, width, yuv) = np.shape(imrgb)
+    for y in xrange(height):
+        for x in xrange(width):
+            imrgb[y][x] = np.dot(M, imrgb[y][x])
+    return imrgb
+
+
+
--- a3_test.py
+++ a3_test.py
@@ -3,116 +3,81 @@ import numpy as np
 import matplotlib.pyplot as plt
 import a3
 import matplotlib.cm as cm
-import imageIO as myImageIO 
+import imageIO as myImageIO
 
-## No need to touch this class
-#class myImageIO:
-#  @staticmethod
-#  def imread(path='in.png'):
-#    from scipy import misc
-#    return (misc.imread(path).astype(float)/255)**2.2
-#
-#  @staticmethod
-#  def imreadg(path='in.png'):
-#    from scipy import misc
-#    return (misc.imread(path).astype(float)/255)
-
-#  @staticmethod
-#  def imwrite(im_in, path):
-#    from scipy import misc
-#    im_in[im_in<0]=0
-#    im_in[im_in>1]=1
-#    return misc.imsave(path, (255*(im_in**(1/2.2))).astype('uint8))
-
-#  @staticmethod
-#  def imwriteg(im_in, path):
-#    from scipy import misc
-#    im_in[im_in<0]=0
-#    im_in[im_in>1]=1
-#    return misc.imsave(path, (255*im_in).astype('uint8'))
-  
-#  @staticmethod
-#  def thresh(im_in):
-#    im_in[im_in<0]=0
-#    im_in[im_in>1]=1
-#    return im_in 
-
-## Test case ## 
+## Test case ##
 ## Feel free to change the parameters or use the impulse as input
 
 def test_box_blur():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   out=a3.boxBlur(im, 7)
-  myImageIO.imwrite(out, 'my_boxblur.png')
+  myImageIO.imwrite(out, 'test_boxBlur.png')
 
 def test_convolve_gauss():
-
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   gauss3=np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])
   kernel=gauss3.astype(float)
   kernel=kernel/sum(sum(kernel))
   out=a3.convolve(im, kernel)
-  myImageIO.imwrite(out, 'my_gaussblur.png')
+  myImageIO.imwrite(out, 'test_convolve.png')
 
 def test_convolve_deriv():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   deriv=np.array([[-1, 1]])
   out=a3.convolve(im, deriv)
-  myImageIO.imwrite(out, 'my_deriv.png')
+  myImageIO.imwrite(out, 'test_convolvDeriv.png')
 
 def test_convolve_Sobel():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   Sobel=np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
   out=a3.convolve(im, Sobel)
-  myImageIO.imwrite(out, 'my_Sobel.png')
+  myImageIO.imwrite(out, 'test_sobel.png')
 
 def test_grad():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   out=a3.gradientMagnitude(im)
-  myImageIO.imwrite(out, 'my_gradient.png')
+  myImageIO.imwrite(out, 'test_magnitude.png')
 
 def test_horigauss():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   kernel=a3.horiGaussKernel(2,3)
   out=a3.convolve(im, kernel)
-  myImageIO.imwrite(out, 'my_horigauss.png')
+  myImageIO.imwrite(out, 'test_horiGauss.png')
 
 def test_gaussianBlur():
-  im=myImageIO.imread('images/pru.png')
-  out=a3.gaussianBlur(im, 2,3)
-  myImageIO.imwrite(out, 'my_gaussBlur.png')
-
+  im=myImageIO.imread('pru.png')
+  out=a3.gaussianBlur(im, 2, 3)
+  myImageIO.imwrite(out, 'test_gaussBlur.png')
 
 def test_gauss2D():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   out=a3.convolve(im, a3.gauss2D())
-  myImageIO.imwrite(out, 'my_gauss2DBlur.png')
+  myImageIO.imwrite(out, 'test_gauss2DBlur.png')
 
 def test_equal():
-  im=myImageIO.imread('images/pru.png')
+  im=myImageIO.imread('pru.png')
   out1=a3.convolve(im, a3.gauss2D())
   out2=a3.gaussianBlur(im,2, 3)
   res=abs(out1-out2);
   return (sum(res.flatten())<0.1)
-  
+
 def test_unsharpen():
-  im=myImageIO.imread('images/zebra.png')
+  im=myImageIO.imread('zebra.png')
   out=a3.unsharpenMask(im, 1, 3, 1)
-  myImageIO.imwrite(out, 'my_unsharpen.png')
+  myImageIO.imwrite(out, 'test_unsharpMask.png')
 
 def test_bilateral():
-  im=myImageIO.imread('images/lens-3-med.png', 1.0)
-  out=a3.bilateral(im, 0.1, 1.4)
-  myImageIO.imwrite(out, 'my_bilateral.png', 1.0)
-  
+  im=myImageIO.imread('lens-3-med.png', 1.0)
+  out=a3.bilateral(im, 0.3, 1.4)
+  myImageIO.imwrite(out, 'test_bilateral.png', 1.0)
 
 def test_bilaYUV():
-  im=myImageIO.imread('images/lens-3-med.png', 1.0)
-  out=a3.bilaYUV(im, 0.1, 1.4, 6)
-  myImageIO.imwrite(out, 'my_bilaYUV.png', 1.0)
-  
+  im=myImageIO.imread('lens-3-small.png', 1.0)
+  out=a3.bilaYUV(im, 0.3, 1.4, 6)
+  myImageIO.imwrite(out, 'test_bilateralYUV.png', 1.0)
+
 def impulse(h=100, w=100):
-    out=constantIm(h, w, 0.0)
+    out=myImageIO.constantIm(h, w, 0.0)
     out[h/2, w/2]=1
     return out
 
@@ -120,15 +85,16 @@ def impulse(h=100, w=100):
 #Uncomment the following function to test your code
 
 
-test_box_blur()
-test_convolve_gauss()
-test_convolve_deriv()
-test_convolve_Sobel()
-test_grad()
-test_horigauss()
-test_gaussianBlur()
-test_gauss2D()
-print test_equal()
-test_unsharpen()
-#test_bilateral()
-#test_bilaYUV()
+# test_box_blur()
+# test_convolve_gauss()
+# test_convolve_deriv()
+# test_convolve_Sobel()
+# test_grad()
+# test_horigauss()
+# test_gaussianBlur()
+# test_gauss2D()
+# print test_equal()
+
+# test_unsharpen()
+# test_bilateral()
+test_bilaYUV()
